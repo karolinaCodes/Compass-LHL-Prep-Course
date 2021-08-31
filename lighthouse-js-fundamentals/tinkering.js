29ae04ae6311da10520535200f7e6c380f6e0828
karolinaCodes <karolinaswislocki@hotmail.com>
2021-08-31 18:28:29 -0400

    Second commit

---
+++ lighthouse-js-fundamentals/tinkering.js
@@ -0,0 +1,13 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(2, 4);
+
+console.log(result);
+
+const my_string = "Udacity";
+
+for (let i = 0; i < my_string.length; i++) {
+  console.log(my_string.charCodeAt(i));
+}
