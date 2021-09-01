d688fb5edae8359d324871be747cc63929fdff58
karolinaCodes <karolinaswislocki@hotmail.com>
2021-09-01 14:07:33 -0400

    added madlib function

--- lighthouse-js-fundamentals/tinkering.js
+++ lighthouse-js-fundamentals/tinkering.js
@@ -11,3 +11,17 @@ const my_string = "Udacity";
 for (let i = 0; i < my_string.length; i++) {
   console.log(my_string.charCodeAt(i));
 }
+
+const adjectives = ["amazing", "fun", "entertaining"];
+
+const madLib = `The Intro to JavaScript course is ${
+  adjectives[Math.floor(Math.random() * 3)]
+}. James and Julia are so ${
+  adjectives[Math.floor(Math.random() * 3)]
+}. I cannot wait to work through the rest of this ${
+  adjectives[Math.floor(Math.random() * 3)]
+} content!`;
+
+console.log(madLib);
+
+// switch between the 3, cannot repeat.
