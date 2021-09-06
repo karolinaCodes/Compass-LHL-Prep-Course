31e844b1c26b8c408870b5316a8bd94ad54ffba7
karolinaCodes <karolinaswislocki@hotmail.com>
2021-09-06 08:42:57 -0400

    Added build triangle quiz

--- lighthouse-js-fundamentals/udacity/functions.js
+++ lighthouse-js-fundamentals/udacity/functions.js
@@ -40,3 +40,30 @@ function isThisWorking(input) {
 }
 
 isThisWorking(3);
+
+// Build a triangle Quiz (Scope)
+/*What the code should do:
+1-take in the widest lenth(max)
+2-have a loop that ouputs a string up to max times
+3-have a ftn that creates a string the # of times you input 
+4-for loop 
+5-holder/accumulator variable that holds the string and return that variable.*/
+
+// creates a line of * for a given length
+function makeLine(length) {
+  var line = "";
+  for (var j = 1; j <= length; j++) {
+    line += "* ";
+  }
+  return line + "\n";
+}
+
+function buildTriangle(widestWidth) {
+  var output = "";
+  for (var i = 1; i <= widestWidth; i++) {
+    output += makeLine(i);
+  }
+  return output;
+}
+
+console.log(buildTriangle(14));
