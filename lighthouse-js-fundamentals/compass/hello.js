9b6977e0f66fae3e965935e254e7aa5c352999fb
karolinaCodes <karolinaswislocki@hotmail.com>
2021-09-06 13:59:37 -0400

    added even.js file

--- lighthouse-js-fundamentals/compass/hello.js
+++ lighthouse-js-fundamentals/compass/hello.js
@@ -13,3 +13,14 @@ const sayHello = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+//console.log vs return
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+};
+sayHelloToConsole("John");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+};
+const greeting = returnSayHello("John");
