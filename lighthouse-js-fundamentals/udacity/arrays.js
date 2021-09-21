4ca77cf6579a0375b8b9da494f4f4535f0e02a95
Karolina <karolinaswislocki@hotmail.com>
2021-09-21 19:04:47 -0400

    added arrays quizzes

--- lighthouse-js-fundamentals/udacity/arrays.js
+++ lighthouse-js-fundamentals/udacity/arrays.js
@@ -1,4 +1,64 @@
-//UdaciFamily
+//UdaciFamily Quiz
 
 // var udaciFamily=["Julia", "James", "Karolina"];
 // console.log(udaciFamily);
+
+// Colors of the rainbow Quiz
+// var rainbow = ["Red", "Orange", "Blackberry", "Blue"];
+// rainbow.splice(2, 1, "Yellow", "Green");
+// rainbow.splice(5, 0, "Purple");
+
+// //Quidditch Cup Quiz
+
+// var team = [
+//   "Oliver Wood",
+//   "Angelina Johnson",
+//   "Katie Bell",
+//   "Alicia Spinnet",
+//   "George Weasley",
+//   "Fred Weasley",
+//   "Harry Potter",
+// ];
+
+// function hasEnoughPlayers(team) {
+//   if (team.length >= 7) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(hasEnoughPlayers(team));
+
+// //Joining the Crew Quiz
+
+// var captain = "Mal";
+// var second = "Zoe";
+// var pilot = "Wash";
+// var companion = "Inara";
+// var mercenary = "Jayne";
+// var mechanic = "Kaylee";
+
+// var crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+// var doctor = "Simon";
+// var sister = "River";
+// var shepherd = "Book";
+
+// // your code goes here
+
+// crew.push(doctor, sister, shepherd);
+
+//Another Type of Loop
+
+// var test = [
+//   12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 19, 300, 3775, 299, 36, 209, 148,
+//   169, 299, 6, 109, 20, 58, 139, 59, 3, 1, 139,
+// ];
+
+// test.forEach(function (element, index) {
+//   if (element % 3 === 0) {
+//     test[index] += 100;
+//   }
+// });
+
+// console.log(test);
