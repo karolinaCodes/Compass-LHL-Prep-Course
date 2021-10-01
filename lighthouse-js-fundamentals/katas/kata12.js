cbb42a950db4c8732c29a484d098fc9a613a5432
Karolina <karolinaswislocki@hotmail.com>
2021-10-01 15:57:59 -0400

    finishes kata 12

--- lighthouse-js-fundamentals/katas/kata12.js
+++ lighthouse-js-fundamentals/katas/kata12.js
@@ -1,12 +1,40 @@
 // The Great Codeville Bake-off
 
 const chooseRecipe = function (bakeryA, bakeryB, recipes) {
-  for (let i<)
+  //arrays of recipe matches for each bakery
+  const bakeryAMatches = [];
+  const bakeryBMatches = [];
+  let recipeMatch = "No recipes matched";
+
+  //loops through recipes and calls ingredientCheck helper ftn. ingredientCheck checks whether the bakery has the ingredient for that recipe, if it does, it adds it to the respective bakeryXMatches array
+  for (let i = 0; i < recipes.length; i++) {
+    if (ingredientCheck(bakeryA, recipes[i].ingredients)) {
+      bakeryAMatches.push(recipes[i].name);
+    }
+    if (ingredientCheck(bakeryB, recipes[i].ingredients)) {
+      bakeryBMatches.push(recipes[i].name);
+    }
+  }
+  //compares the recipe matches of bakery A and bakery B to find which recipe they have in common and returns that recipe name
+  for (let i = 0; i < bakeryAMatches.length; i++) {
+    for (let j = 0; j < bakeryBMatches.length; j++) {
+      if (bakeryAMatches[i] === bakeryBMatches[j]) {
+        recipeMatch = bakeryAMatches[i];
+      }
+    }
+  }
+  return recipeMatch;
 };
 
-/////********programming trick !!! helper function */
-const ingredientCheck = function (recipeIngredients) {
-  //recipe.ingredients array as input array
+//compares (using nested loop) ingredients from the recipe to the ingredients the bakery has and returns true if it does (so then the recipe name is added to the respective bakeryXMatches array)
+const ingredientCheck = function (bakeryIngredients, recipeIngredients) {
+  for (let i = 0; i < recipeIngredients.length; i++) {
+    for (let j = 0; j < bakeryIngredients.length; j++) {
+      if (recipeIngredients[i] === bakeryIngredients[j]) {
+        return true;
+      }
+    }
+  }
 };
 
 let bakeryA = ["saffron", "eggs", "tomato paste", "coconut", "custard"];
