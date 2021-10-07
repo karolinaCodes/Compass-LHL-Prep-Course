73258d61f1675335e1017be95615b13b8d8ea06f
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-10-07 16:33:11 -0400

    kata15

--- lighthouse-js-fundamentals/katas/kata15.js
+++ lighthouse-js-fundamentals/katas/kata15.js
@@ -1,4 +1,35 @@
-let num = 2.13 - 2;
-console.log(num.toFixed(2));
-console.log(typeof num.toFixed(2));
-console.log(Math.floor(num / 2));
+// Organizing Instructors
+
+const organizeInstructors = function (instructors) {
+  let organizedInstructors = {};
+  //loops through instructors array, and destructures the course and name from each object
+  for (let i = 0; i < instructors.length; i++) {
+    const { course } = instructors[i];
+    const { name } = instructors[i];
+
+    //checks whether the course key in already in the organizedInstructors array, if it is, added the name to the key array, if it isn't, adds the course as a new property and adds an array with the instructors name
+    if (organizedInstructors[course]) {
+      organizedInstructors[course].push(name);
+    } else {
+      organizedInstructors[course] = [name];
+    }
+  }
+  return organizedInstructors;
+};
+
+console.log(
+  organizeInstructors([
+    { name: "Samuel", course: "iOS" },
+    { name: "Victoria", course: "Web" },
+    { name: "Karim", course: "Web" },
+    { name: "Donald", course: "Web" },
+  ])
+);
+console.log(
+  organizeInstructors([
+    { name: "Brendan", course: "Blockchain" },
+    { name: "David", course: "Web" },
+    { name: "Martha", course: "iOS" },
+    { name: "Carlos", course: "Web" },
+  ])
+);
