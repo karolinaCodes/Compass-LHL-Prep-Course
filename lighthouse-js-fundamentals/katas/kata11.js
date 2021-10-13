693285632977fe0691f40b44f4b4b661e93d3495
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-10-13 17:08:39 -0400

    completed kata 11

--- lighthouse-js-fundamentals/katas/kata11.js
+++ lighthouse-js-fundamentals/katas/kata11.js
@@ -1,34 +1,47 @@
 // Bouncy Castles
-//cones, spheres and prisms
-
-//total volume of an attraction
-
-// Use the value below whenever you need the value of Pi
-
-//UNSOLVED
 
 const PI = 3.14159;
 
 const sphereVolume = function (radius) {
   let volume = (4 / 3) * PI * Math.pow(radius, 3);
+  return volume;
 };
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
 
 const coneVolume = function (radius, height) {
-  let volume = ((1 / 3) * PI(radius)) ^ 2(height);
+  let volume = (1 / 3) * (PI * Math.pow(radius, 2)) * height;
+  return volume;
 };
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
 
 const prismVolume = function (height, width, depth) {
-  // Probably here too!
+  let volume = width * depth * height;
+  return volume;
 };
 
 console.log(prismVolume(3, 4, 5) === 60);
 
+//calculate the total volume of an attraction
 const totalVolume = function (solids) {
-  // Code here? Yup!
+  let totalVolume = 0;
+
+  for (let i = 0; i < solids.length; i++) {
+    if (solids[i].type === "sphere") {
+      totalVolume += sphereVolume(solids[i].radius);
+    } else if (solids[i].type === "cone") {
+      totalVolume += coneVolume(solids[i].radius, solids[i].height);
+    } else if (solids[i].type === "prism") {
+      totalVolume += prismVolume(
+        solids[i].height,
+        solids[i].width,
+        solids[i].depth
+      );
+    }
+  }
+
+  return totalVolume;
 };
 
 const largeSphere = {
--- lighthouse-js-fundamentals/katas/kata20.js
+++ lighthouse-js-fundamentals/katas/kata20.js
@@ -1,25 +1,31 @@
 //Kata 20 - Taxicab Geometry
+//note: solution in based on the assumption that when the taxi cab is at position (0,0) it is pointing north-east/ 45 degrees.
 
-// console.log(blocksAway(["left", 1, "right", 1, "left", 1, "right", 1, "left", 1, "right", 1]));
+//note: (not included in my github gist)- if the input has more than 4 consecutive left/right turns the function wouldn't work, didn't determine that calculation or a calculation pattern for more than 4 consecutive turns
+
 const blocksAway = function (directions) {
   let coordinates = {
     east: 0,
     north: 0,
   };
 
+  //keeps track of how many consecutive left or rights turns the cab has made since the calculation for the coordintaes is depedant on whether the cab has made previous left/ right turns
   let left = 0;
   let right = 0;
-
   let previousDirection;
 
+  //loops through directions array, if the element is a number skip it, if the previous direction is not the same as the current direction that taxi cab must go then reset that counters so that the next calculation for which ever direction will be for one(left)/ one (right)
   for (let i = 0; i < directions.length; i++) {
-    if (
-      directions[i] !== previousDirection &&
-      typeof directions[i] !== "number"
-    ) {
+    if (typeof directions[i] === "number") {
+      continue;
+    }
+
+    if (directions[i] !== previousDirection) {
       left = 0;
       right = 0;
     }
+    //calls the respective function that calculates the coordinates dependning on the direction and whether the same direction was previously driven (consecutively).
+    //increase the counter for left/ right direction and saves the current direction in previousDirection variable
     if (directions[i] === "right" && right === 0) {
       oneRight(directions[i + 1], coordinates);
       right++;
@@ -58,6 +64,7 @@ const blocksAway = function (directions) {
   return coordinates;
 };
 
+//Right
 function oneRight(num, coordinates) {
   coordinates.east += num;
 }
@@ -71,8 +78,7 @@ function threeRights(num, coordinates) {
 }
 
 function fourRights(num, coordinates) {
-  coordinates.east -= num;
-  coordinates.north -= num;
+  coordinates.north += num;
 }
 
 //Left
@@ -89,13 +95,8 @@ function threeLefts(num, coordinates) {
 }
 
 function fourLefts(num, coordinates) {
-  coordinates.east -= num;
-  coordinates.north -= num;
+  coordinates.east += num;
 }
-
-//1,3
-//3,3
-//0,0
 
 console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
 console.log(
@@ -115,3 +116,8 @@ console.log(
   ])
 );
 console.log(blocksAway(["left", 3, "right", 1, "right", 3, "right", 1]));
+
+// console.log(
+//   blocksAway(["left", 3, "right", 1, "right", 3, "right", 1, "left", 4])
+// );
+//extra testing input
